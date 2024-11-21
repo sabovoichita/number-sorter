8c23dc7707b772a9b6776a286c002a775e3b674b
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-21 17:36:54 +0100

    9. Update your .map() callback to call the Number() function. Pass dropdown.value to that function call. Open the Console tab to see that your inputValues is an array of numbers now.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
   event.preventDefault();
   let inputValues = [...document.getElementsByClassName("values-dropdown")].map(
-    (dropdown) => dropdown.value
+    (dropdown) => Number(dropdown.value)
   );
   console.log(inputValues);
 };
