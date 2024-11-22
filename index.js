86d17fd7f12cc54dd0c2b2078a2f4e1247ceb9e0
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-22 14:29:12 +0100

    27. Like a bubble sort, a selection sort needs to iterate through the array. Declare a for loop to do so.

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ const bubbleSort = (array) => {
   return array;
 };
 
-const selectionSort = (array) => {};
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {}
+};
 
 sortButton.addEventListener("click", sortInputArray);
