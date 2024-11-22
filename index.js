c0a5fb9a52d2c5d39670ce880d47bc52cb3e17c8
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-22 14:25:58 +0100

    24. Click your Sort button to see your bubble sort algorithm in action! If you open the console, you can watch the steps the algorithm takes. Now that you have confirmed it works, remove your console.log() call.

--- index.js
+++ index.js
@@ -23,14 +23,14 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(
-        "array: ",
-        array,
-        "array j: ",
-        array[j],
-        "array[] j+1",
-        array[j + 1]
-      );
+      // console.log(
+      //   "array: ",
+      //   array,
+      //   "array j: ",
+      //   array[j],
+      //   "array[] j+1",
+      //   array[j + 1]
+      // );
       if (array[j] > array[j + 1]) {
         const temp = array[j];
         array[j] = array[j + 1];
